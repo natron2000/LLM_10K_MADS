cdea0e245c77d35224e9a145ec83a7f9dcee7e5f
ndestou <nathan.destouches@gmail.com>
2025-04-05 13:18:04 -0500

    Update GUI to let user choose specific ticker symbol or change the number of documents

--- cap_4_chat.py
+++ cap_4_chat.py
@@ -4,6 +4,7 @@
 """
 
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 import time
 from langchain.prompts import PromptTemplate
@@ -34,32 +35,49 @@ def setup_retrieval_qa(collection_name="k-10s", persist_directory="./chroma_lang
                        llm_model=OLLAMA_MODEL, template="""Use the following pieces of context to answer the 
                        question at the end. If you don't know the answer, just say that you don't know, don't try to make up 
                        an answer. Use 4 sentences maximum. Keep the answer as concise as possible. {context} Question: 
-                           {question} Helpful Answer:"""):
+                           {question} Helpful Answer:""", ticker = 'ALL', k = 2):
     embeddings = OllamaEmbeddings(model=embedding_model)
     db = Chroma(collection_name=collection_name, embedding_function=embeddings, persist_directory=persist_directory)
     llm = OllamaLLM(model=llm_model)
     QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
-    qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever(search_kwargs={"k": 8}), return_source_documents=True, 
-                                           chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+
+    if ticker == 'ALL':
+        qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever(search_kwargs={"k": k}), return_source_documents=True,
+                                               chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+    else:
+        qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever(search_kwargs={"k": k,"filter":{"ticker":ticker}}), return_source_documents=True,
+                                               chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+
     return qa_chain
+
+def get_tickers(collection_name="k-10s", persist_directory="./chroma_langchain_db", embedding_model=OLLAMA_EMBEDDING_MODEL):
+    db = Chroma(collection_name=collection_name, embedding_function=OllamaEmbeddings(model=embedding_model), persist_directory=persist_directory)
+    return list(set([x.get('ticker') for x in db.get(include = ['metadatas']).get('metadatas')]))
 
 def process_query(qa_chain, question):
     result = qa_chain.invoke({"query": question})
     answer = result['result'].split("</think>\n\n")[1].strip()
-    return result
+    return answer
 
 def main():
     start_ollama()
     qa_chain = setup_retrieval_qa()
+    ticker, k = 'ALL', 2
+
+    tickers_list = get_tickers()
+
     #Add tinker here#
     def submit():
         prompt = prompt_entry.get("1.0", tk.END).strip()
 
+        # If User Make A Change Requiring Re-Running setup_retrieval_qa
+        if ticker != ticker_var.get() or k == num_docs_var.get():
+            qa_chain = setup_retrieval_qa(ticker = ticker_var.get(), k = num_docs_var.get())
+
         response = process_query(qa_chain, prompt)
 
         #This is where u can adjust result to trouble shoot
-        result_text.set(f"{
-            [response['source_documents'][i].metadata['ticker'] for i in range(len(response['source_documents']))]}")
+        result_text.set(f"{response}")
         
     root = tk.Tk()
     root.title("10-K SEC LLM")
@@ -69,7 +87,18 @@ def main():
     tk.Label(root, text="Enter Question:").grid(row=0, column=0, sticky="w")
     prompt_entry = tk.Text(root, height=5, width=40)
     prompt_entry.grid(row=1, column=0, columnspan=2, pady=5)
-    
+
+    tk.Label(root, text="Select Ticker (ALL if want to look at all documents):").grid(row=2, column=0, sticky="w")
+    ticker_var = tk.StringVar()
+    ticker_dropdown = ttk.Combobox(root, textvariable=ticker_var, values=tickers_list)
+    ticker_dropdown.grid(row=2, column=1, pady=5)
+    ticker_dropdown.current(0)
+
+    # Number of documents selection
+    tk.Label(root, text="Number of Documents To Retrieve:").grid(row=4, column=0, sticky="w")
+    num_docs_var = tk.IntVar(value=2)
+    tk.Spinbox(root, from_=1, to=100, textvariable=num_docs_var, width=5).grid(row=4, column=1, pady=5)
+
     tk.Button(root, text="Submit", command=submit).grid(row=5, column=0, columnspan=2, pady=10)
     
     result_frame = tk.Frame(root, height=100, width=380, relief=tk.SUNKEN, borderwidth=2)
@@ -84,8 +113,8 @@ def main():
     
     
     
-qa_chain = setup_retrieval_qa() #Delete
-test=process_query(qa_chain, 'how has covid impacted our business') #Delete
+#qa_chain = setup_retrieval_qa() #Delete
+#test=process_query(qa_chain, 'how has covid impacted our business') #Delete
 
 if __name__ == "__main__":
     main()
